53b6ba10235fb1b844a3d11c1f7da22f44467306
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2024-09-07 02:09:44 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    assetPrefix: './', // Ensures assets are correctly referenced
-};
-
-export default nextConfig;
